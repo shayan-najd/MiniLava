2758e27be61248ecf6b3ed199e033d5db526eb8b
Shayan Najd <sh.najd@gmail.com>
2015-06-17 00:16:21 +0200

    Deleting Constructive Analysis, Retime, and transfering operators

--- Lava.hs
+++ Lava.hs
@@ -1,24 +1,24 @@
 module Lava
   ( module Lava.Signal
   , module Lava.Generic
-  , module Lava.Operators
+--  , module Lava.Operators
   , module Lava.Combinational
   , module Lava.Sequential
   , module Lava.SequentialConstructive
-  , module Lava.ConstructiveAnalysis
+--  , module Lava.ConstructiveAnalysis
 --  , module Lava.Test
 --  , module Lava.Property
-  , module Lava.Retime
+--  , module Lava.Retime
   , module Lava.Vhdl
   )
  where
 
 import Lava.Signal
 import Lava.Generic
-import Lava.Operators
+-- import Lava.Operators
 import Lava.Combinational
 import Lava.Sequential
 import Lava.SequentialConstructive
-import Lava.ConstructiveAnalysis
-import Lava.Retime
+-- import Lava.ConstructiveAnalysis
+-- import Lava.Retime
 import Lava.Vhdl
